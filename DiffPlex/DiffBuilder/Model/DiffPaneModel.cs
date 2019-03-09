ae10fe5f2277d44c22dba2db8976184b573cd1a0
CMSQATeam <43958307+CMSQATeam@users.noreply.github.com>
2019-03-09 16:23:29 +0530

    Update DiffPaneModel.cs

--- DiffPlex/DiffBuilder/Model/DiffPaneModel.cs
+++ DiffPlex/DiffBuilder/Model/DiffPaneModel.cs
@@ -2,7 +2,12 @@ using System.Collections.Generic;
 
 namespace DiffPlex.DiffBuilder.Model
 {
-    
+    /// <summary>
+        /// Learn to create the systems
+        /// </summary>
+        /// <description>
+        /// ![abc](3dgames.jpg)
+        /// </description>
     public class DiffPaneModel
     {
         
