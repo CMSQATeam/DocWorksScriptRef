eb0571d6e5916da762c7af8184546f9008b128a2
CMSQATeam <cmsqa123@gmail.com>
2019-03-09 15:17:19 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/Model/ClassProperty.cs
+++ DiffPlex/DiffBuilder/Model/ClassProperty.cs
@@ -2,31 +2,32 @@ namespace DiffPlex.DiffBuilder.Model
 {
     
     /// <summary>
-    /// Learn to create the systems
-    /// </summary>
-    /// <description>
-    /// ![abc](angeler_5c78de84bb171e0bd0de07cd.jpg)
-    /// </description>
-    public class ClassProperty
+        /// Learn to create the systems
+        /// </summary>
+        /// <description>
+        /// ![abc](angeler_5c78de84bb171e0bd0de07cd.jpg)
+        /// </description>
+            public class ClassProperty
     {
         
         /// <summary>
-        /// Presented in 6 phases
-        /// </summary>
-        /// <description>
-        /// [5c7674e2bf9f0a0194bc7676](2D01_5c7674e2bf9f0a0194bc7676.cs)
-        /// </description>
-        public int NumberA { get; set; }
+                /// Presented in 6 phases
+                /// </summary>
+                /// <description>
+                /// [5c7674e2bf9f0a0194bc7676](2D01_5c7674e2bf9f0a0194bc7676.cs)
+                /// </description>
+                        public int NumberA { get; set; }
 
         
         /// <summary>
-        /// This project will teach you an array
-        /// </summary>
-        /// <description>
-        /// []()
-        /// </description>
-        public int NumberB { get; set; }
+                /// This project will teach you an array
+                /// </summary>
+                /// <description>
+                /// []()
+                /// </description>
+                        public int NumberB { get; set; }
 
+        
         
         
         public int NumberC { get; set; }
