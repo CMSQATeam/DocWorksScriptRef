285be2166747e7f661fb1420687c87984cb80dcb
CMSQATeam <43958307+CMSQATeam@users.noreply.github.com>
2019-03-23 15:10:06 +0530

    Update IDiffer.cs

--- DiffPlex/IDiffer.cs
+++ DiffPlex/IDiffer.cs
@@ -4,7 +4,7 @@ using DiffPlex.Model;
 namespace DiffPlex
 {
     ///     <summary>
-        ///     Provides methods for generate differences between texts - update time 14:27
+        ///     Provides methods for generate differences between texts - update time 15:10
         ///     </summary>
             public interface IDiffer
     {
